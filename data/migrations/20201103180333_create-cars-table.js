eca55d0e8be065e4409c8e687eeaa5c16e56912d
Suzanne Cabral <suzannecabral@live.com>
2020-11-03 18:15:34 -0800

    Added migration rules, testing

--- data/migrations/20201103180333_create-cars-table.js
+++ data/migrations/20201103180333_create-cars-table.js
@@ -9,33 +9,41 @@ exports.up = function(knex) {
 
     //vin
     //req, text, unique, 17 characters
-    
+    tbl.integer('vin', 17)
+      .notNullable()
+      .unique();
 
     //make
     //req, text, 50 chars
-
+    tbl.text('make', 50)
+      .notNullable();
 
     //model
     //req, text, 50 chars
-
+    tbl.text('model', 50)
+      .notNullable();
 
     //mileage
     //req, integer
+    tbl.integer('mileage')
+      .notNullable();
 
 
     //OPTIONAL----------
 
     //transmissionType
     //text, 50 chars
-
+    tbl.text('transmissionType', 50);
 
     //titleStatus
     //text, 50 chars
+    tbl.text('titleStatus', 50);
 
-    
+
   });
 };
 
 exports.down = function(knex) {
-  
+  //take out all changes above
+  return knex.schema.dropTableIfExists('cars');
 };
