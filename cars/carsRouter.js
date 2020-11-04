1dab4d95544a205009d1c99b4d6a5ce7e66574c4
Suzanne Cabral <suzannecabral@live.com>
2020-11-04 15:41:37 -0800

    Added validation functions

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -6,6 +6,64 @@ const router = express.Router();
 //----------------------------
 //  middleware validation
 //----------------------------
+const validCarId = (req, res, next) => {
+  const {id} = req.params;
+  
+  //lookup item by id, use DB helper method
+  //returns a promise
+  //resolves to 1 object from db or none
+  router.get(id)
+      .then(data=>{
+          //if there is data returned
+          if(data){
+              //then it passes the check, continue
+              req.car= data;
+              next();
+          }else{
+          //no matching object, not found
+              res.status(404).json({message:`No car found with id: ${id}`});
+          }
+      })
+      .catch(err=>{
+          console.log(err);
+          res.status(500).json({message:`Server error validating car ID`});
+      })
+};
+
+//validate input
+//req:
+
+//   vin      txt (17)
+//   make     txt (50)
+//   model    txt (50)
+//   mileage  num (any)
+
+const validCarInput= (req, res, next) => {
+  //there is a body to the message
+  if(Object.keys(req.body).length>0){
+
+    //all req fields are present
+    if(req.body.vin && req.body.make && req.body.model && req.body.mileage){
+      next();
+      console.log("Car input validated");
+    }else{
+      // /* required data field is missing */
+      res.status(400).json({message:`Required fields: vin, make, model, mileage`});
+    }
+      //body contains both column1 and column
+      //also check for foreign key in this block if needed
+      // if(req.body.column1 && req.body.column2){
+      //     //for testing:
+      //     //res.status(200).json({message:`Item input validated`});
+      //     //passes the check, continue
+      //     next();
+      // }else{
+      //     res.status(400).json({message:`column1 and column2 are required`});
+      // }
+  }else{
+      res.status(400).json({message:`No car information provided`});
+  }
+}
 
 
 //----------------------------
@@ -35,7 +93,7 @@ router.get('/',(req,res,next)=>{
 //  GET /:id
 //----------------------------
 
-router.get('/:id',(req,res,next)=>{
+router.get('/:id', validCarId ,(req,res,next)=>{
   const { id } = req.params;
 
   //select * from posts where id=id
@@ -57,7 +115,22 @@ router.get('/:id',(req,res,next)=>{
 //  POST /
 //----------------------------
 
-router.post('/',(req,res,next)=>{
+router.post('/', validCarInput, (req,res,next)=>{
+
+  const carData = req.body;
+
+  //need validation
+
+  db('cars')
+    .insert(carData)
+    .then(car => {
+      //returns id of newly created entry
+      res.status(200).json({ data:car });
+    })
+    .catch(err=>{
+      console.log('error: ', err);
+      res.status(500).json({ message: err.message });
+    });
 
 });
 
